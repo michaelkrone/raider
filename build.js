2437c087cb31dd48ab6a27b22867a820a792597c
Michael Krone <mk@gastro-instruments.de>
2016-06-01 23:59:46 +0200

    chore(build): add script for keeping the datafiles clean, making it easy to add new words

---
+++ build.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+'use strict';
+
+const fs = require('fs');
+const _ = require('lodash');
+
+const words = {
+	adjectives: require('./data/adjectives'),	
+	adverbs: require('./data/adverbs'),
+	animals: require('./data/animals')
+}; 
+
+_.forEach(words, build);
+_.forEach(words, write);
+
+function build(data) {
+	data.words = _.chain(data.words)
+		.map(w => w.toLowerCase())
+		.map(w => w.trim())
+		.uniq()
+		.sort()
+		.value();
+}
+
+function write(words, type) {
+	const fileName = `./data/${type}.json`;
+	
+	try {
+		fs.writeFileSync(fileName, JSON.stringify(words, null, '\t'));
+		console.log('Data written to %s', fileName);
+	} catch(e) {
+		console.error('Cannot write %s data file to %s', type, fileName);
+		throw e;
+	}
+}
